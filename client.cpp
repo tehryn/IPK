564d7321c2bebb367bc590d713f0e3ffdbbd51be
tehryn <thetehryn@gmail.com>
2017-03-11 16:57:26 +0100

    update

--- client.cpp
+++ client.cpp
@@ -14,6 +14,10 @@
 using namespace std;
 
 class Arguments {
+private:
+    bool cmd_set    = false;
+    bool r_path_set = false;
+    bool l_path_set = false;
 public:
     string command;
     string remote_path;
@@ -24,12 +28,53 @@ public:
 Arguments::Arguments(int argc, char **argv) {
     vector<string> args(argv, argv + argc);
     for (int i = 1; i < argc; i++) {
-        if (args[i] == "-r") {
+        if (this->cmd_set == false) {
+            this->cmd_set = true;
+            if (args[i] == "del") {
+                this->command = "del";
+            }
+            else if (args[i] == "get") {
+                this->command = "get";
+            }
+            else if (args[i] == "put") {
+                this->command = "put";
+            }
+            else if (args[i] == "lst") {
+                this->command = "lst";
+            }
+            else if (args[i] == "mkd") {
+                this->command = "mkd";
+            }
+            else if (args[i] == "rmd") {
+                this->command = "rmd";
+            }
+            else {
+                throw invalid_argument("Invalid command\n");
+            }
         }
-        else if (args[i] == "-p") {
+        else if (this->r_path_set == false) {
+            this->r_path_set = true;
+            this->remote_path = args[i];
+        }
+        else if (this->l_path_set == false) {
+            this->l_path_set = true;
+            this->local_path = args[i];
         }
         else {
-            throw invalid_argument("Arguments of program are invalid\n");
+            throw invalid_argument("Too many arguments\n");
         }
     }
+    if (this->command == "put" and l_path_set == false) {
+        throw invalid_argument("Set local path when using put\n");
+    }
+}
+
+int main(int argc, char **argv) {
+    Arguments *args;
+    try {
+        args = new Arguments(argc, argv);
+    } catch (invalid_argument& e) {
+        cerr << e.what();
+    }
+    cout << args->command << " " << args->remote_path << " " << args->local_path << "\n";
 }
