e3477cb75acb3fec925de8fce5d28dee6fa3e81e
tehryn <thetehryn@gmail.com>
2017-03-15 17:09:32 +0100

    update

--- server.cpp
+++ server.cpp
@@ -29,60 +29,113 @@ public:
     string   root_folder = ".";
     unsigned port        = 6677;
     Arguments(int argc, char **argv);
-    void ld_data(int fd);
 };
 
 class Request {
 private:
     size_t content_start = 1;
-    void set_head(string src) {
-        this->content_start = src.find("\r\n\r\n") + 5;
-        this->head = src.substr(0, this->content_start - 5);
+    size_t byte_read     = 0;
+    bool set_head(string src) {
+        this->content_start = src.find("\r\n\r\n");
+        if (this->content_start != src.npos) {
+            this->content_start += 4;
+            this->head = src.substr(0, this->content_start);
+            return false;
+        }
+        return true;
     }
 
-    void set_command(string src) {
-        this->command = src.substr(0, src.find(' '));
+    bool set_command(string src) {
+        size_t idx = src.find(' ');
+        if (idx != src.npos) {
+            this->command = src.substr(0, idx);
+            return false;
+        }
+        return true;
     }
 
-    void set_path(string src) {
-        size_t tmp = src.find(' ') + 1;
-        this->path = src.substr(tmp, src.find(' ', tmp));
+    bool set_path(string src) {
+        size_t idx = src.find(' ');
+        if (idx != src.npos) {
+            idx++;
+            size_t idx2 = src.find(' ', idx);
+            if (idx2 != src.npos) {
+                this->path = src.substr(idx, idx2 - idx);
+                return false;
+            }
+        }
+        return true;
     }
-    void proc_ld_data();
+
+    bool set_len(string src) {
+        size_t idx  = src.find("Content-Length: ");
+        size_t idx2 = src.find("\r\n", idx);
+        if (idx == src.npos || idx2 == src.npos) {
+            return true;
+        }
+        idx += 16;
+        try {
+            this->content_len = stoi(src.substr(idx, idx2-idx));
+        } catch (exception &e) {
+            return true;
+        }
+        return false;
+    }
+
+    bool proc_ld_data();
 public:
     string command       = "";
     string path          = "";
     string head          = "";
     size_t content_len   =  0;
-    vector<char> content;
+//    vector<char> content;
+    string content       = "";
     void ld_data(int fd);
 };
 
 void Request::ld_data(int fd) {
-/*    size_t idx = message.find("\r\n\r\n");
-    if (idx == message.npos) {
-        throw invalid_argument("HTTP head is missing.\n");
-    }
-    this->set_head(message);
-    this->set_command(message);
-    this->set_path(message);*/
     char buffer[256];
-    unsigned len = 0;
     DEBUG_LINE("Reading socket:");
+    unsigned len;
     while ((len = recv(fd, buffer, 256, 0)) > 0) {
+        this->byte_read += len;
         for (unsigned i = 0; i < len; i++) {
             this->content.push_back(buffer[i]);
             DEBUG_INLINE(buffer[i]);
         }
+        if (this->set_head(this->content) == false) {
+            break;
+        }
     }
     DEBUG_LINE("--END--");
-    return;
+    if (this->proc_ld_data() == true) {
+        throw new invalid_argument("Socket has invalid header!");
+    }
+    DEBUG_LINE("Command:");
+    DEBUG_LINE(this->command);
+    DEBUG_LINE("Path:");
+    DEBUG_LINE(this->path);
+    DEBUG_LINE("Header:");
+    DEBUG_LINE(this->head);
+    DEBUG_INLINE("Content-Length: ");
+    DEBUG_LINE(to_string(this->content_len));
 }
-/*
-Request::Request(int fd) {
 
+bool Request::proc_ld_data() {
+    if (set_command(this->head)) {
+        return true;
+    }
+    else if (set_path(this->head)){
+        return true;
+    }
+    if (command == "PUT") {
+        if (set_len(this->head)) {
+            return true;
+        }
+    }
+    return false;
 }
-*/
+
 
 Arguments::Arguments(int argc, char **argv) {
     vector<string> args(argv, argv + argc);
@@ -158,9 +211,6 @@ int main(int argc, char **argv) {
         sockcomm = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
         if (sockcomm > 0) {
             req.ld_data(sockcomm);
-            DEBUG_LINE(req.head);
-            DEBUG_LINE(req.command);
-            DEBUG_LINE(req.path);
             if (send(sockcomm, buffer, strlen(buffer), 0) < 0) {
                 cerr << "Unable to send message.\n";
                 return 1;
